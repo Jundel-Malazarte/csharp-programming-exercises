c15dac39bc8e4ebc3ab618664c6663fd7cdda1a1
Jundel Malazarte <Jundelmalazarte348@gmail.com>
2024-11-04 23:49:11 +0800

    C# Testing exercises

---
+++ hello/hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Hello_World {
+
+    public static void Main() {
+        Console.WriteLine("Hello World!");
+        
+    }
+}
---
+++ inputs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+					
+public class Input_Program
+{
+	public static void Main()
+	{
+		String fname, lname;
+		
+		Console.WriteLine("Input your firstname: ");
+		fname = Console.ReadLine();
+		
+		Console.WriteLine("Input your lastname: ");
+		lname = Console.ReadLine();
+	
+		Console.WriteLine("\nYour fullname is " + fname + " & " + lname);
+		
+		
+	}
+}
